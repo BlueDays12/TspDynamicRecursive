951fbe50638195a05bc82b1f9f781a3493f79d85
BlueDays12 <38577522+BlueDays12@users.noreply.github.com>
2019-12-18 17:39:00 -0800

    second

--- src/TspDynamicRecursive.java
+++ src/TspDynamicRecursive.java
@@ -1,11 +1,21 @@
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadMXBean;
 import java.util.Scanner;
 
 public class TspDynamicRecursive {
+    static ThreadMXBean bean = ManagementFactory.getThreadMXBean( );
+    static String ResultsFolderPath = "C:\\Users\\msvte\\Desktop\\Test"; // pathname to results folder
+    static FileWriter resultsFile;
+    static PrintWriter resultsWriter;
+
+    static int numberOfTrials = 3;
     static int cost = 0;
     static int N = 200;
     static int[] completed = new int[N];
     static int[][] costMatrix = new int[N][N];
-    static int nodes;
+    static int nodes = 256;
 
 
     // To create elements in my array instead of hard coding
@@ -37,7 +47,7 @@ public class TspDynamicRecursive {
         int i, j;
 
         // Create a random number for the nodes
-        nodes = (int)(5 * Math.random());
+        //nodes = (int)(5 * Math.random());
         System.out.println("Nodes: " + nodes);
 
         // Create a random value for the costMatrix at the
@@ -84,14 +94,54 @@ public class TspDynamicRecursive {
         }
         if (min != 1000)
             cost += kmin;
+
         return next;
     }
 
-    public static void main (String[] args) {
-        //getInput();
+    static void program(String resultsFileName) {
+
+        // To open a file to write to
+        try {
+            resultsFile = new FileWriter(ResultsFolderPath + resultsFileName);
+            resultsWriter = new PrintWriter(resultsFile);
+        } catch (Exception e) {
+            System.out.println("*****!!!!!  Had a problem opening the results file " + ResultsFolderPath + resultsFileName);
+            return;
+        }
+        ThreadCpuStopWatch stopwatch = new ThreadCpuStopWatch(); // for timing an entire set of trials
+
+        long elapsedTime = 0;
+        System.gc();
+        stopwatch.start(); // Start timer in nano secs
+
         create();
         System.out.println("Path: ");
         minCost(0);
         System.out.println("Minimum cost: " + cost);
+
+        resultsWriter.printf("Minimum cost: \n", cost);
+
+        elapsedTime = stopwatch.elapsedTime();
+
+        System.out.println("Time: " + elapsedTime);
+
+        // Output for testing
+
+        resultsWriter.println("#Path     Node    AvgTime"); // # marks a comment in gnuplot data
+        resultsWriter.flush();
+
+
+
+        resultsWriter.printf("%-5d %20d \n", nodes, elapsedTime);
+        resultsWriter.flush();
+    }
+
+    public static void main (String[] args) {
+        System.out.println("X    Time");
+
+        // getInput();
+        // create();
+        program("lab8.txt");
+
     }
 }
